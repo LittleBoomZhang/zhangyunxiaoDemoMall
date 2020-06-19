747c0c704669455c490328ee2e6ba34b4b777926
zhangyunxiao <zhangyunxiao@rfchina.com>
2020-06-19 17:47:13 +0800

    商品列表静态完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,36 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import GoodsList from './../views/GoodsList'
-import Title from '@/views/Title'
-import Img from '@/views/Image'
-import Cart from '@/views/Cart'
 
 Vue.use(Router)
 
 export default new Router({
-  mode: 'hash',
   routes: [
     {
-      path: '/goods',
+      path: '/',
       name: 'GoodsList',
-      component: GoodsList,
-      children:[
-        {
-          path: 'title',
-          name: 'title',
-          component: Title
-        },
-        {
-          path: 'img',
-          name: 'img',
-          component: Img
-        }
-      ]
-    },
-    {
-      path: '/cart',
-      name: 'Cart',
-      component: Cart,
+      component: GoodsList
     }
   ]
 })
